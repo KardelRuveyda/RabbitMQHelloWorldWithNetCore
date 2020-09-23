c8d48d022c51a908c9b88f8a7b4f924ace060cfc
KardelRuveyda <ruveydakardelcetin@gmail.com>
2020-09-24 00:23:27 +0300

    RabbitMQConsumer

--- RabbitMQConsumer/Program.cs
+++ RabbitMQConsumer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbitMQConsumer
 {
@@ -6,7 +9,35 @@ namespace RabbitMQConsumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            //Rabbitmqye ulaşmak için
+
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672/%2F");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume("hello", true, consumer);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var bodyByte = ea.Body.ToArray();
+
+                        var message = Encoding.UTF8.GetString(bodyByte);
+
+                        Console.WriteLine("Mesaj alındı : " + message);
+                        Console.ReadLine();
+                    };
+
+                    Console.WriteLine("Çıkış yapmak için tıklayınız.");
+                    Console.ReadLine();
+
+                }
+            }
         }
     }
 }
--- RabbitMQPublisher/Program.cs
+++ RabbitMQPublisher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQPublisher
 {
@@ -6,7 +8,34 @@ namespace RabbitMQPublisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            //Rabbitmqye ulaşmak için
+
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672/%2F");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //durable false yapılırsa, restart yapılırsa mesaj gider.
+                    //True yapılırsa fiziksel liste yazar.
+                    //false : memoryde dursun daha hızlı yazma imkanı.
+                    channel.QueueDeclare("hello", false, false, false, null);
+
+                    string Message = "Hello World";
+
+                    var bodyByte = Encoding.UTF8.GetBytes(Message);
+
+                    //mesajlar byte dizisi olmalı
+                    channel.BasicPublish("", routingKey: "hello", null, body: bodyByte);
+                    Console.WriteLine("Mesajınız Gönderilmiştir. ");
+
+                }
+
+                Console.WriteLine("Çıkış yapmak için tıklayınız.");
+                Console.ReadLine();
+
+            }
         }
     }
 }
